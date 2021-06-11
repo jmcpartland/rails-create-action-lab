6f169ebcd4eae72f990f50b5590385b1d6154bc6
Joe McPartland <joemcpartland@gmail.com>
2021-06-11 12:44:37 -0600

    commit tree 5960e2d6e21965c7352aeb829cfbd484e5b7f257

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
-  
+    # before_action :set_student, only: [:show, :create]
+
+
   def index
     @students = Student.all
   end
@@ -12,6 +14,24 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
+
+
+  # private
+
+  # def student_params
+  #   params.require.permit(:first_name, :last_name)
+  # end
+
+  # def set_student
+  #   @student = Student.find(params[:id])
+  # end
 
 end
